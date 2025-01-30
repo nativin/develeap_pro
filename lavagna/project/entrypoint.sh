37b48ea636288bc102233a8a2c66e53159aed176
Your Name <you@example.com>
2025-01-30 22:16:28 +0200

    working_on_local

--- lavagna/project/entrypoint.sh
+++ lavagna/project/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-while ! nc -z db 3306; do echo "Waiting for MySQL..."; sleep 1; done
-exec java "-Ddatasource.dialect=MYSQL" \
-    "-Ddatasource.url=jdbc:mysql://db:3306/lavagna?autoReconnect=true&useSSL=false" \
-    "-Ddatasource.username=lavagna_user" \
-    "-Ddatasource.password=lavagna_pass" \
-    "-Dspring.profiles.active=dev" \
-    -jar lavagna.war
+
+# Wait for MySQL to be ready
+while ! nc -z db 3306; do 
+  echo "Waiting for MySQL..."; 
+  sleep 1; 
+done
+
+# Run Lavagna with the provided environment variables
+exec java -Ddatasource.dialect=$DATASOURCE_DIALECT \
+    -Ddatasource.url=$DATASOURCE_URL \
+    -Ddatasource.username=$DATASOURCE_USERNAME \
+    -Ddatasource.password=$DATASOURCE_PASSWORD \
+    -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
+    -jar lavagna-jetty-console.war
